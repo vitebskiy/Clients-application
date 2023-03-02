72d9150e1f981573a4031e81c3c3a6e0027024d2
Miwgan <115034339+vitebskiy@users.noreply.github.com>
2023-03-02 22:16:44 +0300

    Добавил headerи и section Клиенты

---
+++ client/js/index.js
@@ -0,0 +1,11 @@
+import { createClientsHeader } from "./createHeader.js"
+import { createClientsSection } from "./createClientsSection.js"
+
+const createApp = () => {
+  const header = createClientsHeader()
+  const clientSection = createClientsSection()
+
+  document.body.append(header, clientSection.main)
+}
+
+createApp()
